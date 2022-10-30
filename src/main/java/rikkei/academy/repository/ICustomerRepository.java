f1f48149e92ca197d1f044ec834a91c0db8607cf
NGUYEN_CHIEN <nguyenchien.0790@gmail.com>
2022-10-30 18:05:42 +0700

    lan 1

---
+++ src/main/java/rikkei/academy/repository/ICustomerRepository.java
@@ -0,0 +1,12 @@
+package rikkei.academy.repository;
+
+
+import rikkei.academy.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
+
+
